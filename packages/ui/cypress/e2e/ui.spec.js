072e87c070f399d6531af107edeceeb8d835467e
Thomas Rich <tnrich@teselagen.com>
2023-06-28 17:07:27 -0700

    adding cypress workflow

--- packages/ui/cypress/e2e/ui.spec.js
+++ packages/ui/cypress/e2e/ui.spec.js
@@ -3,7 +3,6 @@ describe("zoomCircularView 1.spec", function () {
     cy.visit("");
     //a bunch of things should exist/not exist
     cy.get("div:contains(Zoink)")
-
   });
   it(`should work 2`, function () {
     cy.visit("");
