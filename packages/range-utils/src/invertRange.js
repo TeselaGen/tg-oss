3b24bb676924cd257d58198512ed8af2c31be18f
Thomas Rich <tnrich@teselagen.com>
2023-07-11 11:27:01 -0700

    testing prettier

--- packages/range-utils/src/invertRange.js
+++ packages/range-utils/src/invertRange.js
@@ -1,21 +1,21 @@
-import normalizePositionByRangeLength from './normalizePositionByRangeLength';
-import provideInclusiveOptions from './provideInclusiveOptions';
+import normalizePositionByRangeLength from "./normalizePositionByRangeLength";
+import provideInclusiveOptions from "./provideInclusiveOptions";
 export default provideInclusiveOptions(invertRange);
 
 function invertRange(rangeOrCaret, rangeMax) {
-	if (rangeOrCaret.start > -1) {
-	    const start = rangeOrCaret.end + 1;
-	    const end = rangeOrCaret.start - 1;
-	    return {
-	        start: normalizePositionByRangeLength(start, rangeMax, false),
-	        end: normalizePositionByRangeLength(end, rangeMax, false),
-	    }
-	} else {
-		if (rangeOrCaret > -1) {
-			return {
-		        start: normalizePositionByRangeLength(rangeOrCaret, rangeMax, false),
-		        end: normalizePositionByRangeLength(rangeOrCaret-1, rangeMax, false),
-		    }
-		}
-	}
+  if (rangeOrCaret.start > -1) {
+    const start = rangeOrCaret.end + 1;
+    const end = rangeOrCaret.start - 1;
+    return {
+      start: normalizePositionByRangeLength(start, rangeMax, false),
+      end: normalizePositionByRangeLength(end, rangeMax, false)
+    };
+  } else {
+    if (rangeOrCaret > -1) {
+      return {
+        start: normalizePositionByRangeLength(rangeOrCaret, rangeMax, false),
+        end: normalizePositionByRangeLength(rangeOrCaret - 1, rangeMax, false)
+      };
+    }
+  }
 }
