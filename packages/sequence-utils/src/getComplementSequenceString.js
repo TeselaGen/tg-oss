62dd54e07f1fb9bc978afe4cfa19b82c225c0298
sunm19810-pki <meijian.sun@perkinelmer.com>
2023-07-20 22:55:36 +0800

    support RNA sequence

--- packages/sequence-utils/src/getComplementSequenceString.js
+++ packages/sequence-utils/src/getComplementSequenceString.js
@@ -1,12 +1,14 @@
 import DNAComplementMap from "./DNAComplementMap";
+import { merge } from "lodash";
 
 
 // ac.throw([ac.string,ac.bool],arguments);
-export default function getComplementSequenceString(sequence) {
+export default function getComplementSequenceString(sequence, isRna) {
   // ac.throw([ac.string],arguments);
   let complementSeqString = "";
+  const complementMap = isRna ? merge(DNAComplementMap, { a: 'u', A: 'U'}) : DNAComplementMap;
   for (let i = 0; i < sequence.length; i++) {
-    let complementChar = DNAComplementMap[sequence[i]];
+    let complementChar = complementMap[sequence[i]];
     if (!complementChar) {
       complementChar = sequence[i];
       // throw new Error('trying to get the reverse compelement of an invalid base');
