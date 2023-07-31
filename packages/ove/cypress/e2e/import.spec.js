17aca66c257dd94eccb24c1cf2e29f66b9e1abb8
sunm19810-pki <meijian.sun@perkinelmer.com>
2023-07-31 20:02:38 +0800

    add tests for RNA file importing

--- packages/ove/cypress/e2e/import.spec.js
+++ packages/ove/cypress/e2e/import.spec.js
@@ -14,6 +14,55 @@ describe("import", function () {
     cy.get(".veCircularView").should("exist");
     cy.get(".veLinearView").should("not.exist");
   });
+
+  it("import tool should be able to import a genbank file containing ds-RNA file", function () {
+    cy.get(".veCircularView").should("exist");
+    cy.get(".veLinearView").should("not.exist");
+    cy.uploadFile(`[data-test="veImportTool"]`, "RNA_double_stranded.gb");
+    cy.contains("Sequence Imported").should("exist");
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(100);
+    cy.contains("onSave Callback").should("not.exist");
+    cy.contains("gacgucuuaugacaacuuga").should("exist");
+    cy.contains("cugcagaauacuguugaacu").should("exist");
+  });
+
+  it("import tool should be able to import a genbank file containing ss-RNA file", function () {
+    cy.get(".veCircularView").should("exist");
+    cy.get(".veLinearView").should("not.exist");
+    cy.uploadFile(`[data-test="veImportTool"]`, "RNA_single_stranded.gb");
+    cy.contains("Sequence Imported").should("exist");
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(100);
+    cy.contains("onSave Callback").should("not.exist");
+    cy.contains("gacgucuuaugacaacuuga").should("exist");
+    cy.contains("cugcagaauacuguugaacu").should("not.exist");
+  });
+
+  it("import tool should be able to import a genbank file containing ds-DNA file", function () {
+    cy.get(".veCircularView").should("exist");
+    cy.get(".veLinearView").should("not.exist");
+    cy.uploadFile(`[data-test="veImportTool"]`, "DNA_double_stranded.gb");
+    cy.contains("Sequence Imported").should("exist");
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(100);
+    cy.contains("onSave Callback").should("not.exist");
+    cy.contains("gacgtcttatgacaacttga").should("exist");
+    cy.contains("ctgcagaatactgttgaact").should("exist");
+  });
+
+  it("import tool should be able to import a genbank file containing ss-DNA file", function () {
+    cy.get(".veCircularView").should("exist");
+    cy.get(".veLinearView").should("not.exist");
+    cy.uploadFile(`[data-test="veImportTool"]`, "DNA_single_stranded.gb");
+    cy.contains("Sequence Imported").should("exist");
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(100);
+    cy.contains("onSave Callback").should("not.exist");
+    cy.contains("gacgtcttatgacaacttga").should("exist");
+    cy.contains("ctgcagaatactgttgaact").should("not.exist");
+  });
+
   it("import tool should be able to import a genpep file", function () {
     cy.get(".veCircularView").should("exist");
     cy.get(".veRowItemSequenceContainer");
