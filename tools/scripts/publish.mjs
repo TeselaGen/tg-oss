f6a5652e14f0cc0a3a386bccb47005596dedfb23
smeng9 <38666763+smeng9@users.noreply.github.com>
2023-09-13 13:47:15 -0500

    fix package type if includes both es and cjs

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -85,7 +85,6 @@ if (!version || version === "undefined") {
   json = JSON.parse(readFileSync(`package.json`).toString());
   try {
     json.version = version;
-    json.type = "commonjs";
     json.license = "MIT";
     json.dependencies = { ...deps, ...json.dependencies };
     writeFileSync(`package.json`, JSON.stringify(json, null, 2));
