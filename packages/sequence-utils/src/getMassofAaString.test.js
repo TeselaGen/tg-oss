3b18cd36b781e3c3a36168870323cbe02bac6807
Thomas Rich <tnrich@teselagen.com>
2023-10-13 11:28:19 -0700

    updating veCircularViewTextWrapper styling; fixing aa weight calculation - https://github.com/TeselaGen/openVectorEditor/issues/916

--- packages/sequence-utils/src/getMassofAaString.test.js
+++ packages/sequence-utils/src/getMassofAaString.test.js
@@ -5,14 +5,15 @@ describe("getMassOfAaString", () => {
   it("an empty string has a mass of 0", () => {
     assert.equal(getMassOfAaString(""), 0);
   });
-  it("A string with one amino acids returns the correct results", () => {
-    assert.equal(getMassOfAaString("T"), 119.1);
-    assert.equal(getMassOfAaString("A"), 89.1);
-    assert.equal(getMassOfAaString("F"), 165.2);
+  it("string with one amino acid returns the correct results", () => {
+    assert.equal(getMassOfAaString("T"), 119.12);
+    assert.equal(getMassOfAaString("A"), 89.09);
+    assert.equal(getMassOfAaString("F"), 165.19);
   });
-  it("A string a long string of amino acids returns the correct results", () => {
-    assert.equal(getMassOfAaString("TAGATAFPFPFPA"), 1510.6);
-    assert.equal(getMassOfAaString("TFPMAV"), 754.8);
-    assert.equal(getMassOfAaString("TFPMAVTAGATAFPFPFPA"), 2265.4);
+  it("a long string of amino acids returns the correct results", () => {
+    assert.equal(getMassOfAaString("TAGATAFPFPFPA"), 1294.45);
+    assert.equal(getMassOfAaString("TFPMAV"), 664.81);
+    assert.equal(getMassOfAaString("TFPMAVTAGATAFPFPFPA"), 1941.25);
+    assert.equal(getMassOfAaString("ARNDCEQGHILKMFPSTWYV"), 2395.71);
   });
 });
