094e67ba8ae43de2a58652da1514460653e29341
Thomas Rich <tnrich@teselagen.com>
2023-10-26 17:15:30 -0700

    updating to add shimGlobal file per https://github.com/TeselaGen/tg-oss/issues/42

--- example-demos/oveViteDemo/vite.config.js
+++ example-demos/oveViteDemo/vite.config.js
@@ -1,6 +1,5 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default {
   plugins: [react()],
@@ -11,11 +10,13 @@ export default {
       }
     }
   },
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {}
-  },
+  // tnw trying to fix this issue by using an init global polyfill https://stackoverflow.com/a/73208485/2178112
+  // instead of using the below code since it caused other issues: https://github.com/TeselaGen/tg-oss/issues/42
+  // define: {
+  //   // By default, Vite doesn't include shims for NodeJS/
+  //   // necessary for segment analytics lib to work
+  //   global: {}
+  // },
   test: {
     environment: "jsdom",
     globals: true
