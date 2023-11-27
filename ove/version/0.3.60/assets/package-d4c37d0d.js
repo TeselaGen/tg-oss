836fac4b839391953e0a5a4ede28df367b488020
tnrich <tnrich@teselagen.com>
2023-11-27 22:17:44 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@4210da525ccbf833df13206e40e4267052369950 🚀

---
+++ ove/version/0.3.60/assets/package-d4c37d0d.js
@@ -0,0 +1,23 @@
+const name = "@teselagen/ui";
+const version = "0.3.51";
+const main = "./src/index.js";
+const exports = {
+  ".": {
+    "import": "./index.es.js",
+    require: "./index.cjs.js"
+  },
+  "./style.css": "./style.css"
+};
+const _package = {
+  name,
+  version,
+  main,
+  exports
+};
+export {
+  _package as default,
+  exports,
+  main,
+  name,
+  version
+};
