8c42a544c43dfad17f614d65c5cc86c63c24894a
Thomas Rich <tnrich@teselagen.com>
2024-02-20 15:16:15 -0300

    tiny

--- packages/ui/cypress/e2e/ExcelTable.spec.js
+++ packages/ui/cypress/e2e/ExcelTable.spec.js
@@ -3,7 +3,15 @@ describe("ExcelTable.spec", () => {
     cy.visit("#/DataTable%20-%20ExcelTable");
     cy.get(`[data-test="tgCell_Thing 1"]:contains(88)`).rightclick();
     cy.contains("Add Row Above").click();
-    
+
+    cy.get(
+      `[data-index="0"] [data-test="tgCell_Thing 1"][data-copy-json="{"__strVal":""}"]]`
+    );
+    cy.get(
+      `[data-index="1"] [data-test="tgCell_Thing 1"][data-copy-json="{"__strVal":"88"}"]]`
+    );
+    cy.get(`[data-index="1"] [data-test="tgCell_Thing 1"]`).dblclick({
+      force: true
+    });
   });
- 
 });
