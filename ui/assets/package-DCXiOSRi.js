aae06ba337a956ce033f058113769195ceb33ab7
tnrich <tnrich@teselagen.com>
2024-04-15 22:11:19 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@1e5c0b753bfc83a9a9b95a3deb9fc5d0779e12b9 🚀

---
+++ ui/assets/package-DCXiOSRi.js
@@ -0,0 +1,23 @@
+const name = "@teselagen/ove";
+const version = "0.4.10";
+const main = "./src/index.js";
+const exports = {
+  ".": {
+    "import": "./index.es.js",
+    require: "./index.cjs.js"
+  },
+  "./style.css": "./style.css"
+};
+const _package = {
+  name,
+  version,
+  main,
+  exports
+};
+export {
+  _package as default,
+  exports,
+  main,
+  name,
+  version
+};
