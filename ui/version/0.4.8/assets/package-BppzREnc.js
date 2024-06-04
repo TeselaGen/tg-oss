0509865fd80c0f001c73fbe6468fc2f1128e46b3
tnrich <tnrich@teselagen.com>
2024-06-04 15:38:42 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@f088e616b594c9bc962259b1d1c97a77488ef390 🚀

---
+++ ui/version/0.4.8/assets/package-BppzREnc.js
@@ -0,0 +1,28 @@
+const name = "@teselagen/uploader";
+const version = "0.2.0";
+const main = "./src/index.js";
+const types = "./index.d.ts";
+const type = "module";
+const exports = {
+  ".": {
+    "import": "./index.es.js",
+    require: "./index.cjs.js"
+  }
+};
+const _package = {
+  name,
+  version,
+  main,
+  types,
+  type,
+  exports
+};
+export {
+  _package as default,
+  exports,
+  main,
+  name,
+  type,
+  types,
+  version
+};
