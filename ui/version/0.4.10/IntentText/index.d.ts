fc0157f62a4c909a4ccd28ece77ffaf9b45957e4
tnrich <tnrich@teselagen.com>
2024-06-05 20:16:00 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@fb106ecff32712ad36175aa326c3d3a486357249 🚀

---
+++ ui/version/0.4.10/IntentText/index.d.ts
@@ -0,0 +1,5 @@
+export default function IntentText({ intent, text, children }: {
+    intent: any;
+    text: any;
+    children: any;
+}): import("react/jsx-runtime").JSX.Element;
---
+++ ui/version/0.4.10/SimpleStepViz.d.ts
@@ -0,0 +1,4 @@
+export default function SimpleStepViz({ steps, ...rest }: {
+    [x: string]: any;
+    steps: any;
+}): import("react/jsx-runtime").JSX.Element;
