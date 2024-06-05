f528401b63e2c4435b776d66e03eead4a3cdf97e
tnrich <tnrich@teselagen.com>
2024-06-05 20:16:06 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@fb106ecff32712ad36175aa326c3d3a486357249 🚀

---
+++ ui/CollapsibleCard/index.d.ts
@@ -0,0 +1,12 @@
+export default function CollapsibleCard({ title, icon, openTitleElements, noCard, className, style, children, initialClosed, toggle, isOpen }: {
+    title: any;
+    icon: any;
+    openTitleElements: any;
+    noCard?: boolean | undefined;
+    className: any;
+    style: any;
+    children: any;
+    initialClosed?: boolean | undefined;
+    toggle: any;
+    isOpen: any;
+}): import("react/jsx-runtime").JSX.Element;
