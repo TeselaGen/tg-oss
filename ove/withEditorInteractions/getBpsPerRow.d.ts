4b388614eeb794a4e87c625d562e4e67b3ac74bc
tnrich <tnrich@teselagen.com>
2024-06-05 20:16:55 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@fb106ecff32712ad36175aa326c3d3a486357249 🚀

---
+++ ove/withEditorInteractions/getBpsPerRow.d.ts
@@ -0,0 +1,9 @@
+export default function getBpsPerRow({ charWidth, width, dimensions: { width: width2 }, marginWidth, sequenceData }: {
+    charWidth?: number | undefined;
+    width?: number | undefined;
+    dimensions?: {
+        width: any;
+    } | undefined;
+    marginWidth?: number | undefined;
+    sequenceData: any;
+}): number;
