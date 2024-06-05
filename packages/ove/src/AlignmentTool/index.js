fb106ecff32712ad36175aa326c3d3a486357249
Thomas Willheim <tnrich@teselagen.com>
2024-06-05 13:13:57 -0700

    adding back dts, converting a few components to fns

--- packages/ove/src/AlignmentTool/index.js
+++ packages/ove/src/AlignmentTool/index.js
@@ -3,14 +3,10 @@ import React from "react";
 // import CutsiteFilter from "../CutsiteFilter";
 // import { Tabs, Tab, Button, InputGroup, Intent } from "@blueprintjs/core";
 
-export default class AlignmentTool extends React.Component {
-  // state={selectedTab: "virtualDigest"}
-  render() {
-    const { height = "100%" } = this.props;
-    return (
-      <div style={{ height, overflow: "auto", padding: 10 }}>
-        <div>Add an alignment file (.gb, .fasta, .ab)</div>
-      </div>
-    );
-  }
-}
+export default ({ height = "100%" }) => {
+  return (
+    <div style={{ height, overflow: "auto", padding: 10 }}>
+      <div>Add an alignment file (.gb, .fasta, .ab)</div>
+    </div>
+  );
+};
--- packages/ui/src/Loading/index.js
+++ packages/ui/src/Loading/index.js
@@ -1,74 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DNALoader from "../DNALoader";
 import "./style.css";
 import { BounceLoader } from "@teselagen/bounce-loader";
 
-export default class Loading extends React.Component {
-  state = {
-    longerThan200MS: false
+const Loading = ({
+  loading,
+  style: userStyle,
+  className,
+  containerStyle = {},
+  children,
+  displayInstantly = false,
+  bounce = false,
+  withTimeout,
+  inDialog,
+  centeredInPage
+}) => {
+  const [longerThan200MS, setLongerThan200MS] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLongerThan200MS(true);
+    }, 200);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  const style = {
+    ...userStyle,
+    ...(inDialog && { minHeight: 120 })
   };
 
-  componentDidMount() {
-    this.timeoutId = setTimeout(() => {
-      this.setState({
-        longerThan200MS: true
-      });
-    }, 200);
-  }
+  const LoaderComp = bounce || inDialog ? BounceLoader : DNALoader;
 
-  componentWillUnmount() {
-    clearTimeout(this.timeoutId);
-  }
-
-  render() {
-    const {
-      loading,
-      style: userStyle,
-      className,
-      containerStyle = {},
-      children,
-      displayInstantly = false,
-      bounce = false,
-      withTimeout,
-      inDialog,
-      centeredInPage
-    } = this.props;
-    const { longerThan200MS } = this.state;
-    const style = {
-      ...userStyle,
-      ...(inDialog && { minHeight: 120 })
-    };
-    const LoaderComp = bounce || inDialog ? BounceLoader : DNALoader;
-    if (loading || !children) {
-      if (
-        !displayInstantly &&
-        !longerThan200MS &&
-        ((!bounce && !inDialog) || withTimeout)
-      ) {
-        return <div />;
-      }
-      return (
-        <div
-          className="tg-loader-container tg-flex justify-center align-center"
-          style={{
-            width: "100%",
-            ...containerStyle,
-            ...(centeredInPage && {
-              width: undefined,
-              zIndex: 20,
-              height: 10,
-              position: "fixed",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, 0)"
-            })
-          }}
-        >
-          <LoaderComp style={style} className={className} />
-        </div>
-      );
-    } else {
-      return children || null;
+  if (loading || !children) {
+    if (
+      !displayInstantly &&
+      !longerThan200MS &&
+      ((!bounce && !inDialog) || withTimeout)
+    ) {
+      return <div />;
     }
+    return (
+      <div
+        className="tg-loader-container tg-flex justify-center align-center"
+        style={{
+          width: "100%",
+          ...containerStyle,
+          ...(centeredInPage && {
+            width: undefined,
+            zIndex: 20,
+            height: 10,
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, 0)"
+          })
+        }}
+      >
+        <LoaderComp style={style} className={className} />
+      </div>
+    );
+  } else {
+    return children || null;
   }
-}
+};
+
+export default Loading;
