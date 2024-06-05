7874d4735fa8c21cb3bd5df87e0e5a8a10d9b8db
tnrich <tnrich@teselagen.com>
2024-06-05 21:46:38 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@71e9429d765a1da9039bf4a42ccc10b25a834847 🚀

---
+++ ui/version/0.4.11/utils/tagUtils.d.ts
@@ -0,0 +1,20 @@
+export function getTagsAndTagOptions(allTags: any): any[];
+export function getKeyedTagsAndTagOptions(tags: any): import('lodash').Dictionary<any>;
+export function getTagColorStyle(color: any): {
+    style: {
+        backgroundColor: any;
+        color: string;
+    };
+} | {
+    style?: undefined;
+};
+export function getTagProps(vals: any): {
+    children: any;
+    style: {
+        backgroundColor: any;
+        color: string;
+    };
+} | {
+    children: any;
+    style?: undefined;
+};
