3c62dc6bd6807253687c32a9e10913cf0c448a6f
tgreen7 <greenmachine777@gmail.com>
2024-06-11 04:40:28 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@afddd538f0ca5e892f25c92d65665b58d66000c9 🚀

---
+++ ui/version/0.4.12/DataTable/SearchBar.d.ts
@@ -0,0 +1,9 @@
+export default SearchBar;
+declare function SearchBar({ reduxFormSearchInput, setSearchTerm, loading, searchMenuButton, disabled, autoFocusSearch }: {
+    reduxFormSearchInput: any;
+    setSearchTerm: any;
+    loading: any;
+    searchMenuButton: any;
+    disabled: any;
+    autoFocusSearch: any;
+}): import("react/jsx-runtime").JSX.Element;
