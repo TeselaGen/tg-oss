5651860cd0dbe321f0546978e5fae1ea05c06bbd
tnrich <tnrich@teselagen.com>
2024-06-13 00:28:28 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@8fc0afc4f0b19b09691b488c3ed4f3ea9f38fb38 🚀

---
+++ ove/version/0.5.16/DigestTool/DigestTool.d.ts
@@ -0,0 +1,11 @@
+import { default as React } from '../../../../node_modules/react';
+export class DigestTool extends React.Component<any, any, any> {
+    constructor(props: any);
+    constructor(props: any, context: any);
+    state: {
+        selectedTab: string;
+    };
+    render(): import("react/jsx-runtime").JSX.Element;
+}
+declare const _default: any;
+export default _default;
