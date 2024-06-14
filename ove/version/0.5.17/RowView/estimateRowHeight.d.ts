0a657ec9c21f167d0f40dd815c2cb205b2d82a48
tnrich <tnrich@teselagen.com>
2024-06-14 15:43:25 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@06f0ba0bacb26303d210f8624b6023f0ee6a1856 🚀

---
+++ ove/version/0.5.17/RowView/estimateRowHeight.d.ts
@@ -0,0 +1,6 @@
+export namespace rowHeights {
+    import primaryProteinSequence = rowHeights.translations;
+    export { primaryProteinSequence };
+}
+declare function _default(props: any): any;
+export default _default;
---
+++ ove/version/0.5.17/redux/utils/addMetaToActionCreators.d.ts
@@ -0,0 +1,1 @@
+export default function addMetaToActionCreators(actions: any, meta: any): {};
