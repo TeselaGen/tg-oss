5e89e98a79738c0a7155c4d155dc7f882545b243
tnrich <tnrich@teselagen.com>
2024-07-01 20:59:44 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@4c09e53ea16c4e3e371153db14f564020f108d38 🚀

--- ove/StatusBar/index.d.ts
+++ ove/StatusBar/index.d.ts
@@ -1,5 +1,6 @@
-export function StatusBar({ disableSetReadOnly, onSave, editorName, showCircularity, showMoleculeType, showReadOnly, showAvailability, showGCContentByDefault, onSelectionOrCaretChanged, GCDecimalDigits, isProtein, beforeReadOnlyChange }: {
+export function StatusBar({ disableSetReadOnly, disableBpEditing, onSave, editorName, showCircularity, showMoleculeType, showReadOnly, showAvailability, showGCContentByDefault, onSelectionOrCaretChanged, GCDecimalDigits, isProtein, beforeReadOnlyChange }: {
     disableSetReadOnly: any;
+    disableBpEditing: any;
     onSave: any;
     editorName: any;
     showCircularity?: boolean | undefined;
