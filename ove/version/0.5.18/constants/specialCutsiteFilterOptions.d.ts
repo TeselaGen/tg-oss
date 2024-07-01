0368dc38c745b36975d2e31c055ac030aae18e7c
tnrich <tnrich@teselagen.com>
2024-07-01 20:59:37 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@4c09e53ea16c4e3e371153db14f564020f108d38 🚀

---
+++ ove/version/0.5.18/constants/specialCutsiteFilterOptions.d.ts
@@ -0,0 +1,31 @@
+declare namespace _default {
+    namespace type2s {
+        let value: string;
+        let label: string;
+        let isSpecialGroup: boolean;
+    }
+    namespace single {
+        let value_1: string;
+        export { value_1 as value };
+        let label_1: string;
+        export { label_1 as label };
+        export let cutsThisManyTimes: number;
+    }
+    namespace double {
+        let value_2: string;
+        export { value_2 as value };
+        let label_2: string;
+        export { label_2 as label };
+        let cutsThisManyTimes_1: number;
+        export { cutsThisManyTimes_1 as cutsThisManyTimes };
+    }
+    namespace triple {
+        let value_3: string;
+        export { value_3 as value };
+        let label_3: string;
+        export { label_3 as label };
+        let cutsThisManyTimes_2: number;
+        export { cutsThisManyTimes_2 as cutsThisManyTimes };
+    }
+}
+export default _default;
---
+++ ove/version/0.5.18/redux/alignments.d.ts
@@ -0,0 +1,9 @@
+export const upsertAlignmentRun: import('redux-act').EmptyActionCreator;
+export const removeAlignmentFromRedux: import('redux-act').EmptyActionCreator;
+export const updateAlignmentViewVisibility: import('redux-act').EmptyActionCreator;
+export const alignmentRunUpdate: import('redux-act').EmptyActionCreator;
+declare function _default(state: {} | undefined, { payload, type }: {
+    payload?: {} | undefined;
+    type: any;
+}): {};
+export default _default;
