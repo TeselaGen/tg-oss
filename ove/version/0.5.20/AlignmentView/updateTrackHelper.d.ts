e691e02e504cb0b611b896af2606b566d5360d37
tnrich <tnrich@teselagen.com>
2024-07-05 19:03:21 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@911177df1f12b2e8c60518c3ba6a7bf59d214f3c 🚀

---
+++ ove/version/0.5.20/AlignmentView/updateTrackHelper.d.ts
@@ -0,0 +1,10 @@
+export function updateTrackHelper({ currentPairwiseAlignmentIndex, pairwiseAlignments, upsertAlignmentRun, hasBeenTrimmed, alignmentId, alignmentTracks, alignmentTrackIndex, update }: {
+    currentPairwiseAlignmentIndex: any;
+    pairwiseAlignments: any;
+    upsertAlignmentRun: any;
+    hasBeenTrimmed: any;
+    alignmentId: any;
+    alignmentTracks: any;
+    alignmentTrackIndex: any;
+    update: any;
+}): void;
---
+++ ove/version/0.5.20/helperComponents/PinchHelper/PinchHelper.d.ts
@@ -0,0 +1,6 @@
+/**
+ * wrapper for the react usePinch gesture
+ * @param {*} children child components to be enveloped
+ * @param {*} onPinch the action to be performed when pinch gesture is registered
+ */
+export default function PinchHelper({ children, onPinch }: any): import("react/jsx-runtime").JSX.Element;
