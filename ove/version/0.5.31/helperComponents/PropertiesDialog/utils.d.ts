41f83d1e1a4c931331bd47d5a00207b972ae4565
tnrich <tnrich@teselagen.com>
2024-08-30 23:17:47 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@6b1e3081c4eae3b03b9bc07b4d59b093f716bac9 🚀

---
+++ ove/version/0.5.31/helperComponents/PropertiesDialog/utils.d.ts
@@ -0,0 +1,5 @@
+export namespace sizeSchema {
+    let path: string;
+    let type: string;
+    function render(val: any, _record: any, i: any, props: any): import("react/jsx-runtime").JSX.Element;
+}
