65b30bf57c2bd1ada9530a605000c5f8a5acecad
tnrich <tnrich@teselagen.com>
2024-10-23 00:03:18 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@42e3e1b1b76322df2d29740de480764ec82262fb 🚀

---
+++ ove/version/0.6.1-beta.1/redux/deletionLayers.d.ts
@@ -0,0 +1,5 @@
+export const deletionLayerClear: import('redux-act').ComplexActionCreator1<any, any, any>;
+export const deletionLayerUpdate: import('redux-act').ComplexActionCreator1<any, any, any>;
+export const deletionLayerDelete: import('redux-act').ComplexActionCreator1<any, any, any>;
+declare const _default: import('redux-act').Reducer<{}, import('../../../../node_modules/redux').AnyAction>;
+export default _default;
---
+++ ove/version/0.6.1-beta.1/redux/primerLengthsToHide.d.ts
@@ -0,0 +1,7 @@
+export const updatePrimerLengthsToHide: import('redux-act').ComplexActionCreator1<any, any, any>;
+export const togglePrimerLengthsToHide: import('redux-act').ComplexActionCreator1<any, any, any>;
+declare const _default: {
+    (newState: {} | undefined, action: any): any;
+    __shouldUseMergedState: boolean;
+};
+export default _default;
