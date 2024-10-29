1a7aab39190dea7b661df45f890c3cb67bb36a19
tnrich <tnrich@teselagen.com>
2024-10-29 16:22:12 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@415f5343ac404d619a283189b192391a4c8c21ce 🚀

---
+++ ui/version/0.6.1/TgSelect/index.d.ts
@@ -0,0 +1,10 @@
+export function createNewOption(newValString: any): {
+    userCreated: boolean;
+    label: any;
+    value: any;
+} | undefined;
+export function simplesearch(needle: any, haystack: any): boolean;
+declare function _default(props: any): import("react/jsx-runtime").JSX.Element;
+export default _default;
+export function renderCreateNewOption(query: any, active: any, handleClick: any): import("react/jsx-runtime").JSX.Element;
+export function itemListPredicate(_queryString: string | undefined, items: any, isSimpleSearch: any): any;
