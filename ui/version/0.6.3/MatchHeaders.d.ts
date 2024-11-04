fc66232003186ead4aefb34ed870a4e5c7b40c48
tnrich <tnrich@teselagen.com>
2024-11-04 22:33:24 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@bce9aae586c1b215827629b71d6009b6afdd7eb2 🚀

---
+++ ui/version/0.6.3/MatchHeaders.d.ts
@@ -0,0 +1,16 @@
+export function MatchHeaders({ onMultiFileUploadSubmit, doAllFilesHaveSameHeaders, csvValidationIssue, ignoredHeadersMsg, searchResults, matchedHeaders, userSchema, reduxFormEntitiesArray, changeForm, datatableFormName, datatableFormNames: _datatableFormNames, setFilesWIssues, filesWIssues, fileIndex }: {
+    onMultiFileUploadSubmit: any;
+    doAllFilesHaveSameHeaders: any;
+    csvValidationIssue: any;
+    ignoredHeadersMsg: any;
+    searchResults: any;
+    matchedHeaders: any;
+    userSchema: any;
+    reduxFormEntitiesArray: any;
+    changeForm: any;
+    datatableFormName: any;
+    datatableFormNames: any;
+    setFilesWIssues: any;
+    filesWIssues: any;
+    fileIndex: any;
+}): import("react/jsx-runtime").JSX.Element;
---
+++ ui/version/0.6.3/MenuBar/index.d.ts
@@ -0,0 +1,29 @@
+import { default as React } from '../../../../node_modules/react';
+export default MenuBar;
+declare class MenuBar extends React.Component<any, any, any> {
+    static defaultProps: {
+        className: string;
+        style: {};
+    };
+    constructor(props: any);
+    hotkeyEnabler: ({ children }?: {
+        children: any;
+    }) => import("react/jsx-runtime").JSX.Element;
+    state: {
+        isOpen: boolean;
+        openIndex: null;
+        helpItemQueryStringTracker: string;
+    };
+    handleInteraction: (index: any) => (newOpenState: any) => void;
+    handleMouseOver: (index: any) => () => void;
+    getAllMenuItems: () => any;
+    addHelpItemIfNecessary: (menu: any, i: any) => any;
+    isTopLevelSearch: boolean | undefined;
+    menuSearchIndex: any;
+    searchInput: HTMLInputElement | undefined;
+    helpItemRenderer: (i: any, b: any) => import("react/jsx-runtime").JSX.Element;
+    handleItemClickOrSelect: (__i: any) => (_i: any) => void;
+    toggleFocusSearchMenu: () => void;
+    render(): import("react/jsx-runtime").JSX.Element;
+    n: HTMLButtonElement | undefined;
+}
