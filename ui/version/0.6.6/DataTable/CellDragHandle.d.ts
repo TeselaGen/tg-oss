f9054d825b6beb2e1c3c9345fc3f73e4dcc8c68f
tnrich <tnrich@teselagen.com>
2024-11-07 21:39:19 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@f0b7ff2fed8738eaa2e50eb863bffe6993e97ebe 🚀

---
+++ ui/version/0.6.6/DataTable/CellDragHandle.d.ts
@@ -0,0 +1,6 @@
+export function CellDragHandle({ thisTable, onDragEnd, cellId, isSelectionARectangle }: {
+    thisTable: any;
+    onDragEnd: any;
+    cellId: any;
+    isSelectionARectangle: any;
+}): import("react/jsx-runtime").JSX.Element;
---
+++ ui/version/0.6.6/DataTable/viewColumn.d.ts
@@ -0,0 +1,12 @@
+export namespace viewColumn {
+    let width: number;
+    let noEllipsis: boolean;
+    let hideInMenu: boolean;
+    let immovable: boolean;
+    let type: string;
+    function render(): import("react/jsx-runtime").JSX.Element;
+}
+export namespace openColumn {
+    export function render_1(val: any, record: any, rowInfo: any, props: any): import("react/jsx-runtime").JSX.Element;
+    export { render_1 as render };
+}
