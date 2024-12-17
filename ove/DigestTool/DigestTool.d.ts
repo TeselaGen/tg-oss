837e925088e55f40c707741bc81d97abad2dfe51
tnrich <tnrich@teselagen.com>
2024-12-17 17:02:49 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@ef11103b5ee338c5f5bb8156f877074a77a97698 🚀

--- ove/DigestTool/DigestTool.d.ts
+++ ove/DigestTool/DigestTool.d.ts
@@ -1,3 +1,3 @@
 export function DigestTool(props: any): import("react/jsx-runtime").JSX.Element;
-declare const _default: any;
+declare const _default: (props: any) => import("react/jsx-runtime").JSX.Element;
 export default _default;
--- ove/helperComponents/PropertiesDialog/index.d.ts
+++ ove/helperComponents/PropertiesDialog/index.d.ts
@@ -1,10 +1,5 @@
-import { default as React } from '../../../../../node_modules/react';
-export class PropertiesDialog extends React.Component<any, any, any> {
-    constructor(props: any);
-    constructor(props: any, context: any);
-    render(): import("react/jsx-runtime").JSX.Element;
-}
-declare const _default: import('../../../../../node_modules/react-redux').ConnectedComponent<typeof PropertiesDialog, {
+export function PropertiesDialog(props: any): import("react/jsx-runtime").JSX.Element;
+declare const _default: import('../../../../../node_modules/react-redux').ConnectedComponent<(props: any) => import("react/jsx-runtime").JSX.Element, {
     [x: string]: any;
 }>;
 export default _default;
