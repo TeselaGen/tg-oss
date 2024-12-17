723919e16ce2d9957a7c5b23cd8b1cf061b2df8e
mfanselmo <martin.f.anselmo@gmail.com>
2024-12-17 20:32:03 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@658d39d3bcadbdef30378e73044f24168efb62a4 🚀

---
+++ ui/version/0.7.9/InfoHelper/index.d.ts
@@ -0,0 +1,19 @@
+declare function _default({ className, content, children, icon, isPopover, isButton, size, isInline, clickable, color, noMarginTop, popoverProps, disabled, displayToSide, style, ...rest }: {
+    [x: string]: any;
+    className: any;
+    content: any;
+    children: any;
+    icon?: string | undefined;
+    isPopover: any;
+    isButton: any;
+    size: any;
+    isInline: any;
+    clickable: any;
+    color: any;
+    noMarginTop: any;
+    popoverProps?: {} | undefined;
+    disabled: any;
+    displayToSide: any;
+    style: any;
+}): import("react/jsx-runtime").JSX.Element;
+export default _default;
