4ceeb08a7e96aa4c218a92135b7cbc867c3684ae
tnrich <tnrich@teselagen.com>
2025-01-03 20:19:12 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@268fecd2d5a5f4586a71cb6ab2890759248fe85f 🚀

---
+++ ove/version/0.7.7/helperComponents/PropertiesDialog/index.d.ts
@@ -0,0 +1,5 @@
+export function PropertiesDialog(props: any): import("react/jsx-runtime").JSX.Element;
+declare const _default: import('../../../../../node_modules/react-redux').ConnectedComponent<(props: any) => import("react/jsx-runtime").JSX.Element, {
+    [x: string]: any;
+}>;
+export default _default;
