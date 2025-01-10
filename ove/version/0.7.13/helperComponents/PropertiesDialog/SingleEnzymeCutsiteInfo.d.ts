6c2b7c423ee7a1be1b5cf0d7b2225bbe43dcdddc
tnrich <tnrich@teselagen.com>
2025-01-10 18:01:57 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@da43f489feae7b032bfc568b31d5449820cbfbf5 🚀

---
+++ ove/version/0.7.13/helperComponents/PropertiesDialog/SingleEnzymeCutsiteInfo.d.ts
@@ -0,0 +1,12 @@
+export default function SingleEnzymeCutsiteInfo({ cutsiteGroup, enzyme, dispatch, allRestrictionEnzymes, editorName, selectedAnnotationId, allCutsites, filteredCutsites: { cutsitesByName: cutsitesByNameActive } }: {
+    cutsiteGroup: any;
+    enzyme: any;
+    dispatch: any;
+    allRestrictionEnzymes: any;
+    editorName: any;
+    selectedAnnotationId: any;
+    allCutsites: any;
+    filteredCutsites: {
+        cutsitesByName: any;
+    };
+}): import("react/jsx-runtime").JSX.Element;
