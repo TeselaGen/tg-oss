dec86c8370f5d06121938d5f3d364658eef703f4
tnrich <tnrich@teselagen.com>
2025-01-28 19:02:27 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@a8fc851789331c3629e8253b12ce582bf2d48430 🚀

---
+++ ui/assets/package-BETak_8K.js
@@ -0,0 +1,19 @@
+const name = "@teselagen/range-utils";
+const version = "0.3.9";
+const type = "module";
+const dependencies = {
+  "lodash-es": "^4.17.21"
+};
+const _package = {
+  name,
+  version,
+  type,
+  dependencies
+};
+export {
+  _package as default,
+  dependencies,
+  name,
+  type,
+  version
+};
---
+++ ui/assets/package-rX1z2RiH.js
@@ -0,0 +1,26 @@
+const name = "@teselagen/ove";
+const version = "0.7.16";
+const main = "./src/index.js";
+const type = "module";
+const exports = {
+  ".": {
+    "import": "./index.es.js",
+    require: "./index.cjs.js"
+  },
+  "./*": "./*"
+};
+const _package = {
+  name,
+  version,
+  main,
+  type,
+  exports
+};
+export {
+  _package as default,
+  exports,
+  main,
+  name,
+  type,
+  version
+};
