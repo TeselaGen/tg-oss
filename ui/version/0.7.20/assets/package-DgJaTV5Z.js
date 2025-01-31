e3acf374f1516800e7402322042fc0254918c109
tnrich <tnrich@teselagen.com>
2025-01-31 19:20:21 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@13fbe679650bcfbdf5b3d992876874144bbee92c 🚀

---
+++ ui/version/0.7.20/assets/package-DgJaTV5Z.js
@@ -0,0 +1,27 @@
+const name = "@teselagen/file-utils";
+const version = "0.3.18";
+const type = "module";
+const dependencies = {
+  bluebird: "^3.7.2",
+  jszip: "^3.10.1",
+  "lodash-es": "^4.17.21",
+  papaparse: "5.3.2"
+};
+const devDependencies = {
+  "mock-fs": "^5.2.0"
+};
+const _package = {
+  name,
+  version,
+  type,
+  dependencies,
+  devDependencies
+};
+export {
+  _package as default,
+  dependencies,
+  devDependencies,
+  name,
+  type,
+  version
+};
