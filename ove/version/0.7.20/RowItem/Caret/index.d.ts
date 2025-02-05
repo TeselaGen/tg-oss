1031f7ce0d234b0c2b2036ac082ef80b41e3a09d
tnrich <tnrich@teselagen.com>
2025-02-05 19:32:05 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@dd1872ac2615bebd13c0c9f0e18cd82c632dd042 🚀

---
+++ ove/version/0.7.20/RowItem/Caret/index.d.ts
@@ -0,0 +1,15 @@
+export default Caret;
+declare function Caret({ charWidth, row, sequenceLength, caretPosition, isProtein, onClick, isDraggable, leftMargin, onRightClick, style, selectionMessage, className }: {
+    charWidth: any;
+    row: any;
+    sequenceLength: any;
+    caretPosition: any;
+    isProtein: any;
+    onClick: any;
+    isDraggable: any;
+    leftMargin?: number | undefined;
+    onRightClick: any;
+    style: any;
+    selectionMessage: any;
+    className?: string | undefined;
+}): import("react/jsx-runtime").JSX.Element | null;
---
+++ ove/version/0.7.20/redux/lastSavedId.d.ts
@@ -0,0 +1,3 @@
+export const lastSavedIdUpdate: import('redux-act').ComplexActionCreator1<any, any, any>;
+declare const _default: import('redux-act').Reducer<null, import('../../../../node_modules/redux').AnyAction>;
+export default _default;
