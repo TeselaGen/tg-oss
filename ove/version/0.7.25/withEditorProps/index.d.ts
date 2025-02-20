da8cd80ae33d1eb2b410b3b1e934add7a503c65a
tnrich <tnrich@teselagen.com>
2025-02-20 21:51:09 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@7c30d93a40f8e65f27c20ca548764785d9e20363 🚀

---
+++ ove/version/0.7.25/withEditorProps/index.d.ts
@@ -0,0 +1,17 @@
+export function mapDispatchToActions(dispatch: any, ownProps: any): {
+    selectionLayerUpdate: any;
+    caretPositionUpdate: any;
+    dispatch: any;
+};
+export function fakeActionOverrides(): {};
+export function getCombinedActions(editorName: any, actions: any, actionOverrides: any, dispatch: any): {};
+export function getShowGCContent(state: any, ownProps: any): any;
+export function handleSave(props: any): (opts?: {}) => Promise<any>;
+export function handleInverse(props: any): () => false | undefined;
+export function updateCircular(props: any): (isCircular: any) => Promise<void>;
+export function importSequenceFromFile(props: any): (file: any, opts?: {}) => Promise<void>;
+export function exportSequenceToFile(props: any): (format: any, options: any) => void;
+declare const _default: any;
+export default _default;
+export function connectToEditor(fn: any): import('../../../../node_modules/react-redux').InferableComponentEnhancerWithProps<any, any>;
+export const withEditorPropsNoRedux: any;
