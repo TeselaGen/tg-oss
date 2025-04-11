34f5aaba757374f45fa6d749b55f53038c96e76a
jgespinosa10 <30691527+jgespinosa10@users.noreply.github.com>
2025-04-11 12:50:22 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@f6c22577417638d95ba0fc1da0aa79e71803f669 🚀

---
+++ ui/version/0.8.2/assets/package-DEUElk8o.js
@@ -0,0 +1,20 @@
+const name = "@teselagen/sequence-utils";
+const version = "0.3.30";
+const type = "module";
+const dependencies = { "escape-string-regexp": "5.0.0", "jsondiffpatch": "0.4.1", "string-splice": "^1.3.0", "lodash-es": "^4.17.21", "shortid": "2.2.16", "@teselagen/range-utils": "file:../range-utils" };
+const exports = { ".": { "import": "./index.js", "require": "./index.cjs" } };
+const _package = {
+  name,
+  version,
+  type,
+  dependencies,
+  exports
+};
+export {
+  _package as default,
+  dependencies,
+  exports,
+  name,
+  type,
+  version
+};
