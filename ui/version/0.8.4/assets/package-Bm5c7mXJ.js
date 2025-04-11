d25871063deea685ce815bcb70a4b66fc800701a
tnrich <tnrich@teselagen.com>
2025-04-11 04:40:56 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@b1e66060427b612019b63864c4a5a1379ecbae96 🚀

---
+++ ui/version/0.8.4/assets/package-Bm5c7mXJ.js
@@ -0,0 +1,17 @@
+const name = "@teselagen/range-utils";
+const version = "0.3.13";
+const type = "module";
+const dependencies = { "lodash-es": "^4.17.21" };
+const _package = {
+  name,
+  version,
+  type,
+  dependencies
+};
+export {
+  _package as default,
+  dependencies,
+  name,
+  type,
+  version
+};
