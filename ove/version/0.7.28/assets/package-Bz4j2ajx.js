6c258a8127698f1a48d0269adda7d889f67ad97b
tnrich <tnrich@teselagen.com>
2025-04-30 16:26:02 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@3ca3ed98136df17594f1d9571a5ccaadaade4a0d 🚀

---
+++ ove/version/0.7.28/assets/package-Bz4j2ajx.js
@@ -0,0 +1,20 @@
+const name = "@teselagen/file-utils";
+const version = "0.3.20";
+const type = "module";
+const dependencies = { "bluebird": "^3.7.2", "jszip": "^3.10.1", "lodash-es": "^4.17.21", "papaparse": "5.3.2", "buffer": "5.7.1" };
+const devDependencies = { "mock-fs": "^5.2.0" };
+const _package = {
+  name,
+  version,
+  type,
+  dependencies,
+  devDependencies
+};
+export {
+  _package as default,
+  dependencies,
+  devDependencies,
+  name,
+  type,
+  version
+};
