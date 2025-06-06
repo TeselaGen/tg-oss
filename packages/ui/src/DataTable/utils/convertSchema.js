d38b4866baefa32ecb95f82b004b5a473581d36a
Guillermo Espinosa <juan.gmo1997@gmail.com>
2025-06-06 16:48:43 -0400

    rollback

--- packages/ui/src/DataTable/utils/convertSchema.js
+++ packages/ui/src/DataTable/utils/convertSchema.js
@@ -1,27 +1,7 @@
 import { camelCase } from "lodash-es";
 import { startCase, keyBy, map } from "lodash-es";
 
-type Field = {
-  type?: string;
-  displayName?: string;
-  path?: string;
-  filterDisabled?: boolean;
-  sortDisabled?: boolean;
-};
-
-type Schema = { fields: (Field | string)[] };
-
-type CompleteField = Field & {
-  type: string;
-  displayName: string;
-  path: string;
-};
-
-type CompleteSchema = {
-  fields: CompleteField[];
-};
-
-const convertSchema = (schema: Schema): CompleteSchema => {
+function convertSchema(schema) {
   let schemaToUse = schema;
   if (!schemaToUse.fields && Array.isArray(schema)) {
     schemaToUse = {
@@ -32,30 +12,40 @@ const convertSchema = (schema: Schema): CompleteSchema => {
     ...schemaToUse
   };
   schemaToUse.fields = schemaToUse.fields.map((field, i) => {
+    let fieldToUse = field;
     if (typeof field === "string") {
-      return {
+      fieldToUse = {
         displayName: startCase(camelCase(field)),
         path: field,
         type: "string"
       };
-    } else {
-      const fieldToUse = { ...(field as Field) };
-      fieldToUse.type = fieldToUse.type || "string";
-      fieldToUse.displayName =
-        fieldToUse.displayName || startCase(camelCase(fieldToUse.path || ""));
-      // paths are needed for column resizing
-      if (!fieldToUse.path) {
-        fieldToUse.filterDisabled = true;
-        fieldToUse.sortDisabled = true;
-        fieldToUse.path = "fake-path" + i;
-      }
-      return fieldToUse as CompleteField;
+    } else if (!field.type) {
+      fieldToUse = {
+        ...field,
+        type: "string"
+      };
     }
+    if (!fieldToUse.displayName) {
+      fieldToUse = {
+        ...fieldToUse,
+        displayName: startCase(camelCase(fieldToUse.path))
+      };
+    }
+    // paths are needed for column resizing
+    if (!fieldToUse.path) {
+      fieldToUse = {
+        ...fieldToUse,
+        filterDisabled: true,
+        sortDisabled: true,
+        path: "fake-path" + i
+      };
+    }
+    return fieldToUse;
   });
-  return schemaToUse as CompleteSchema;
-};
+  return schemaToUse;
+}
 
-export function mergeSchemas(_originalSchema: Schema, _overrideSchema: Schema) {
+export function mergeSchemas(_originalSchema, _overrideSchema) {
   const originalSchema = convertSchema(_originalSchema);
   const overrideSchema = convertSchema(_overrideSchema);
 
