5a9201752dd812a3259fdb7e5152d5f7ddd472a6
Thomas Willheim <tnrich@teselagen.com>
2025-06-10 11:21:21 -0700

    tiny

--- packages/sequence-utils/src/calculateMeltingTempUtils.test.js
+++ packages/sequence-utils/src/calculateMeltingTempUtils.test.js
@@ -1,12 +1,8 @@
 import assert from "assert";
-import {
-  breslauer1986Tm,
-  nebSantaLucia1998Tm
-} from "./calculateMeltingTempUtils";
+import { idtAllawi1997Tm, rnaXia1998Tm } from "./calculateMeltingTempUtils";
 
 // eatodo - add tests for the two tm functions we're keeping here
 
 describe("", function () {
-
-    // your tests here
+  // your tests here
 });
