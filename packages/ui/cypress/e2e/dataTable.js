99793b24eb6d6293faeaafa3b100d33257be1af4
Thomas Willheim <tnrich@teselagen.com>
2025-06-19 13:40:01 -0700

    fix copy table test

--- packages/ui/cypress/e2e/dataTable.js
+++ packages/ui/cypress/e2e/dataTable.js
@@ -58,14 +58,14 @@ describe("dataTable.spec", () => {
     Cypress._dt_num_entities = 600;
     cy.visit("#/DataTable?pageSize=400"); // must be >200 to hit the table virtualization
     //  - copying a single row (selected or not)
-    cy.get(`[data-test="tgCell_type.special"]`).first().click();
-    cy.focused().type("{meta+a}");
-    cy.focused().type("{ctrl+a}");
+    cy.get(`[name="checkBoxHeader"]`).click({ force: true }); //select all
+    // cy.focused().type("{meta+a}");
+    // cy.focused().type("{ctrl+a}");
     //tnr: typing both so that the hotkey is triggered even when running on tests on linux in CI (maybe it will be solved some day https://github.com/cypress-io/cypress/issues/8961)
-    cy.get(".data-table-container").type("{meta+c}");
-    cy.get(".data-table-container").type("{ctrl+c}");
+    cy.get(".data-table-container").type("{meta+c}", { force: true });
+    cy.get(".data-table-container").type("{ctrl+c}", { force: true });
     cy.contains("Selected rows copied").then(() => {
-      expect(Cypress.__copiedRowsLength).to.eq(400);
+      expect(Cypress.__copiedRowsLength).to.eq(401); // 400 rows + 1 header row
     });
   });
   it(`it can copy a single row, selected rows, or cells to the clipboard`, () => {
