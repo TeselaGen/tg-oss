a8e3f3fc582c3b728eb03e9b131395f9912d7c86
tnrich <tnrich@teselagen.com>
2025-10-01 19:07:12 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@dae879e04dd76d19d09ca269772824a4a5348df0 🚀

---
+++ ui/version/0.10.2/TagSelect/index.d.ts
@@ -0,0 +1,6 @@
+declare function _default({ value, options, onChange }: {
+    value?: {} | undefined;
+    options?: never[] | undefined;
+    onChange: any;
+}): import("react/jsx-runtime").JSX.Element;
+export default _default;
