fc8401c37c61ea93c834d24d0820109807a50715
tnrich <tnrich@teselagen.com>
2025-10-24 17:46:46 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@4aa236bc5f6fe120a7d6b429e7d201a7ff8ff408 🚀

---
+++ ui/version/0.10.8/ResizableDraggableDialog/index.d.ts
@@ -0,0 +1,22 @@
+import { default as React } from '../../../../node_modules/react';
+export default class ResizableDraggableDialog extends React.Component<any, any, any> {
+    constructor(props: any);
+    constructor(props: any, context: any);
+    componentDidMount(): void;
+    resizeObs: ResizeObserver | undefined;
+    state: {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+    };
+    setDefaults: ({ doNotSetXOrWidth }?: {}) => void;
+    onWindowResize: () => void;
+    componentWillUnmount(): void;
+    getWindowWidthAndHeight: () => {
+        windowWidth: number;
+        windowHeight: number;
+    };
+    render(): import("react/jsx-runtime").JSX.Element;
+    containerEl: HTMLDivElement | undefined;
+}
---
+++ ui/version/0.10.8/UploadCsvWizard.d.ts
@@ -0,0 +1,4 @@
+export function PreviewCsvData(props: any): import("react/jsx-runtime").JSX.Element;
+export const SimpleInsertDataDialog: any;
+export default UploadCsvWizardDialog;
+declare const UploadCsvWizardDialog: any;
