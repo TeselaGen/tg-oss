d405da5dc9ae13cfab5e9ba7a7d860f632117c5b
tnrich <tnrich@teselagen.com>
2025-10-27 17:55:09 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@a39ac0cf44a740a6e5eb2b566339f92ac0ca4484 🚀

--- ove/withEditorProps/index.d.ts
+++ ove/withEditorProps/index.d.ts
@@ -4,7 +4,7 @@ export function mapDispatchToActions(dispatch: any, ownProps: any): {
     dispatch: any;
 };
 export function fakeActionOverrides(): {};
-export function getCombinedActions(editorName: any, actions: any, actionOverrides: any, dispatch: any): {};
+export function getCombinedActions(editorName: any, actions: any, actionOverrides: any, dispatch: any, ownProps: any): {};
 export function getShowGCContent(state: any, ownProps: any): any;
 export function handleSave(props: any): (opts?: {}) => Promise<any>;
 export function handleInverse(props: any): () => false | undefined;
