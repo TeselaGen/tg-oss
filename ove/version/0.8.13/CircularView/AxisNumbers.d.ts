72e75d4fc743a43d34668c062495ec82ded67698
tnrich <tnrich@teselagen.com>
2025-11-07 19:09:40 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@cc75c0b826bdc78b24189fe1eba185ee62e0c0cd 🚀

---
+++ ove/version/0.8.13/CircularView/AxisNumbers.d.ts
@@ -0,0 +1,7 @@
+export function AxisNumbers({ rotationRadians, textHeightOffset, annotation, isProtein, hideNumbers }: {
+    rotationRadians: any;
+    textHeightOffset?: number | undefined;
+    annotation: any;
+    isProtein: any;
+    hideNumbers: any;
+}): import("react/jsx-runtime").JSX.Element;
