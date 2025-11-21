368f168767c9f5e2d1be8c7a7600eddf94455c6d
tnrich <tnrich@teselagen.com>
2025-11-21 21:58:07 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@b61e27d845864aff2a73c36a12b43ed3ef55a155 🚀

--- ove/selectors/translationsSelector.d.ts
+++ ove/selectors/translationsSelector.d.ts
@@ -1,4 +1,6 @@
-declare const _default: ((state: any) => any) & import('reselect').OutputSelectorFields<(args_0: any, args_1: unknown, args_2: any, args_3: {
+declare const _default: ((state: any) => {
+    [x: string]: any;
+}) & import('reselect').OutputSelectorFields<(args_0: any, args_1: unknown, args_2: any, args_3: {
     start: number;
     end: number;
     length: number;
@@ -8,7 +10,9 @@ declare const _default: ((state: any) => any) & import('reselect').OutputSelecto
     annotationTypePlural: string;
     isOrf: boolean;
     id: any;
-}[], args_4: any, args_5: any, args_6: any[], args_7: any, args_8: any, args_9: any, args_10: any, args_11: any, args_12: any) => any, {
+}[], args_4: any, args_5: any, args_6: any[], args_7: any, args_8: any, args_9: any, args_10: any, args_11: any, args_12: any) => {
+    [x: string]: any;
+}, {
     clearCache: () => void;
 }> & {
     clearCache: () => void;
