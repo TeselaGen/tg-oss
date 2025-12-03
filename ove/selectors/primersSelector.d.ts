a862cbdae015d3b52cca8930220ecdea1665e238
tnrich <tnrich@teselagen.com>
2025-12-03 17:15:19 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@b5283fda6295c6eeb82851569875ea20b980251f 🚀

--- ove/selectors/primersSelector.d.ts
+++ ove/selectors/primersSelector.d.ts
@@ -1,4 +1,4 @@
-declare const _default: ((state: any) => any) & import('reselect').OutputSelectorFields<(args_0: any) => any, {
+declare const _default: ((state: any) => any) & import('reselect').OutputSelectorFields<(args_0: any, args_1: any) => any, {
     clearCache: () => void;
 }> & {
     clearCache: () => void;
--- ove/selectors/searchLayersSelector.d.ts
+++ ove/selectors/searchLayersSelector.d.ts
@@ -1,4 +1,4 @@
-declare const _default: ((state: any) => any[]) & import('reselect').OutputSelectorFields<(args_0: any, args_1: any, args_2: any, args_3: any, args_4: any, args_5: any, args_6: any, args_7: any, args_8: any) => any[], {
+declare const _default: ((state: any, ...params: any[]) => any[]) & import('reselect').OutputSelectorFields<(...args: readonly unknown[]) => any[], {
     clearCache: () => void;
 }> & {
     clearCache: () => void;
