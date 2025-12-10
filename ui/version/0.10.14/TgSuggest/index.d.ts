12d40a67282d58a7f6c29c53d7d31fb3826d5bab
tnrich <tnrich@teselagen.com>
2025-12-10 19:46:31 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@e079c58a8e78e81f7157a135836347dc71a3ebfb 🚀

---
+++ ui/version/0.10.14/TgSuggest/index.d.ts
@@ -0,0 +1,19 @@
+export default TgSuggest;
+declare function TgSuggest({ disabled, inputProps: _inputProps, intent, isLoading, isSimpleSearch, multi, noResultsText, notCreateable, onBlur, onChange, optionRenderer, options, placeholder, popoverProps: _popoverProps, value, ...rest }: {
+    [x: string]: any;
+    disabled: any;
+    inputProps: any;
+    intent: any;
+    isLoading: any;
+    isSimpleSearch: any;
+    multi: any;
+    noResultsText: any;
+    notCreateable: any;
+    onBlur: any;
+    onChange: any;
+    optionRenderer: any;
+    options: any;
+    placeholder: any;
+    popoverProps: any;
+    value: any;
+}): import("react/jsx-runtime").JSX.Element;
