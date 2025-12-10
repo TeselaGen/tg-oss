e623c9c516171e5e0271f40fddad819ff8bac9c9
tnrich <tnrich@teselagen.com>
2025-12-10 17:06:04 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@303279306aab0f90a6ecc2a4df9c1da836025cb4 🚀

---
+++ ove/version/0.8.23/redux/sequenceDataHistory.d.ts
@@ -0,0 +1,8 @@
+export const addToUndoStack: import('redux-act').ComplexActionCreator1<any, any, any>;
+export const veUndo: import('redux-act').ComplexActionCreator1<any, any, any>;
+export const veRedo: import('redux-act').ComplexActionCreator1<any, any, any>;
+declare const _default: import('redux-act').Reducer<{
+    past: never[];
+    future: never[];
+}, import('../../../../node_modules/redux').AnyAction>;
+export default _default;
