8e40f571c4f2491807ef325ebae0e73b7a7206ec
tnrich <tnrich@teselagen.com>
2026-01-07 21:31:29 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@5bc41decf89c373bb44069add8c82c9af206c6df 🚀

---
+++ ove/version/0.8.27/redux/caretPosition.d.ts
@@ -0,0 +1,4 @@
+export const caretPositionClear: import('redux-act').ComplexActionCreator1<any, any, any>;
+export const caretPositionUpdate: import('redux-act').ComplexActionCreator1<any, any, any>;
+declare const _default: import('redux-act').Reducer<number, import('../../../../node_modules/redux').AnyAction>;
+export default _default;
