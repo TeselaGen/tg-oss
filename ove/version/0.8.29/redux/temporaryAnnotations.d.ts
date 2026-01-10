b848da5a6c77980ee9840c9ae00c12b7aaf38d94
tnrich <tnrich@teselagen.com>
2026-01-10 14:05:25 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@63f93751b81558784d6ad3220499178ab147e505 🚀

---
+++ ove/version/0.8.29/redux/temporaryAnnotations.d.ts
@@ -0,0 +1,6 @@
+export const updateTemporaryAnnotations: import('redux-act').ComplexActionCreator1<any, any, any>;
+declare const _default: {
+    (newState: {} | undefined, action: any): any;
+    __shouldUseMergedState: boolean;
+};
+export default _default;
