7b1de44c1b8df3bcee8cdb2e2ce76ddc03648e9f
James Sun <meijian.sun@revvity.com>
2026-01-13 08:31:20 +0800

    fix annotation lable for non-english characters (#235)
    * fix annotation lable for non-english characters
    
    * update
    
    * revert
    
    * bump version
    
    * update
    
    * fix test

--- packages/ove/src/RowItem/utils.js
+++ packages/ove/src/RowItem/utils.js
@@ -1,14 +1,30 @@
 import { ANNOTATION_LABEL_FONT_WIDTH } from "./constants";
 import { getWidth } from "./getXStartAndWidthOfRowAnnotation";
 
+// Cache canvas context for text measurement
+let measureCanvas;
+export function getAnnotationTextWidth(
+  text,
+  fontSize = ANNOTATION_LABEL_FONT_WIDTH,
+  fontFamily = "monospace"
+) {
+  if (!measureCanvas) {
+    measureCanvas = document.createElement("canvas");
+  }
+  const ctx = measureCanvas.getContext("2d");
+  ctx.font = `${fontSize}px ${fontFamily}`;
+  return ctx.measureText(text).width;
+}
+
 export const doesLabelFitInAnnotation = (
   text = "",
   { range, width },
   charWidth
 ) => {
-  const textLength = text.length * ANNOTATION_LABEL_FONT_WIDTH;
-  const widthMinusOne =
-    (range ? getWidth(range, charWidth, 0) : width) - charWidth;
+  const textLength = getAnnotationTextWidth(text);
+  const widthMinusOne = range
+    ? getWidth(range, charWidth, 0) - ANNOTATION_LABEL_FONT_WIDTH * 2
+    : width - ANNOTATION_LABEL_FONT_WIDTH * 2;
   return widthMinusOne > textLength;
 };
 
