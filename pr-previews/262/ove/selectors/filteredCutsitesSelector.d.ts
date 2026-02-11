f7b5076b25bf4fe7688cbca62ff2c1806262f937
tnrich <tnrich@users.noreply.github.com>
2026-02-11 16:15:05 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@bd864d3e6d9925b79200ed472f98a3b04f96515f 🚀

---
+++ pr-previews/262/ove/selectors/filteredCutsitesSelector.d.ts
@@ -0,0 +1,10 @@
+declare const _default: ((state: any) => {
+    cutsitesByName: {};
+}) & import('reselect').OutputSelectorFields<(args_0: any, args_1: any, args_2: any, args_3: any) => {
+    cutsitesByName: {};
+}, {
+    clearCache: () => void;
+}> & {
+    clearCache: () => void;
+};
+export default _default;
