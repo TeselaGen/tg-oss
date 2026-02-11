f6b7d5efca6b725b4f643c6e13a13eb68cbb8ee8
tnrich <tnrich@users.noreply.github.com>
2026-02-11 15:48:23 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@083a4eb821637f685618d9ec9c4c7377714fc7c9 🚀

---
+++ pr-previews/253/ove/RowItem/utils.d.ts
@@ -0,0 +1,5 @@
+export function getAnnotationTextWidth(text: any, fontFamily?: string): any;
+export function doesLabelFitInAnnotation(text: string | undefined, { range, width }: {
+    range: any;
+    width: any;
+}, charWidth: any): boolean;
---
+++ pr-previews/253/ove/selectors/searchLayersSelector.d.ts
@@ -0,0 +1,6 @@
+declare const _default: ((state: any, ...params: any[]) => any[]) & import('reselect').OutputSelectorFields<(...args: readonly unknown[]) => any[], {
+    clearCache: () => void;
+}> & {
+    clearCache: () => void;
+};
+export default _default;
