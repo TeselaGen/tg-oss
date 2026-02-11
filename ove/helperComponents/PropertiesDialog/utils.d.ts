0290025b53adeeab0700656a5eb15ec418387661
tnrich <tnrich@teselagen.com>
2026-02-11 16:26:55 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@90e066dc581a057db9696b65b324149e762e984a 🚀

--- ove/helperComponents/PropertiesDialog/utils.d.ts
+++ ove/helperComponents/PropertiesDialog/utils.d.ts
@@ -1,6 +1,6 @@
-export function sizeSchema(isProtein: any): {
+export function sizeSchema(): {
     path: string;
     type: string;
-    render: (val: any, _record: any) => import("react/jsx-runtime").JSX.Element;
+    render: (val: any, record: any) => import("react/jsx-runtime").JSX.Element;
 };
 export function getMemoOrfs(editorState: any): any;
