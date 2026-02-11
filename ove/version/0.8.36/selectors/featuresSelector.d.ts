a1bbfb6d131c52362bfeae6912d8f416cc7239f0
tnrich <tnrich@teselagen.com>
2026-02-11 16:01:13 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@df856d2db85e7f3fcd2cac6595b523e50c8901ef 🚀

---
+++ ove/version/0.8.36/selectors/featuresSelector.d.ts
@@ -0,0 +1,6 @@
+declare const _default: ((state: any) => any) & import('reselect').OutputSelectorFields<(args_0: any, args_1: any) => any, {
+    clearCache: () => void;
+}> & {
+    clearCache: () => void;
+};
+export default _default;
