2b8e0bf19d3db1f4571eecf3ad81faac0a1be677
tnrich <tnrich@teselagen.com>
2026-05-05 18:59:24 +0000

    Deploying to gh-pages from @ TeselaGen/tg-oss@d0204ac7fc8b68fe2f44c2befd1a631e0936b10e 🚀

--- ui/DataTable/DisabledLoadingComponent.d.ts
+++ ui/DataTable/DisabledLoadingComponent.d.ts
@@ -3,4 +3,4 @@ declare function DisabledLoadingComponent({ disabled, loading, loadingText }: {
     disabled: any;
     loading: any;
     loadingText: any;
-}): import("react/jsx-runtime").JSX.Element;
+}): import("react/jsx-runtime").JSX.Element | null;
